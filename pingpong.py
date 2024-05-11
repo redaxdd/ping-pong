230936a9c4ac43c6be43a2691b2f63397331b533
redaxdd <168259747+redaxdd@users.noreply.github.com>
2024-05-11 11:20:15 +0500

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -4,6 +4,8 @@ window = display.set_mode((600, 600))
 display.set_caption("ping-pong")
 BG = transform.scale(image.load("background.jpg"), (600, 600))
 clock = time.Clock()
+speed_y = 1
+speed_x = 1
 
 class GameSprite(sprite.Sprite):
     def __init__(self, x, y, w, h, filename, speed=0):
@@ -16,6 +18,7 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
 class Wall(sprite.Sprite):
     def __init__(self, x, y, w, h, color=(12, 79, 79)):
         super().__init__()
@@ -27,41 +30,46 @@ class Wall(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player1(GameSprite):
+    def update():
+        keys = key.get_pressed()
+        if keys[K_w]:
+            if Wall1.rect.y > 0:
+                Wall1.rect.y -= 5 
+        elif keys[K_s]:
+            if Wall1.rect.y < 410:
+                Wall1.rect.y += 5 
+class Player2(GameSprite):
+    def update():
+        keys = key.get_pressed()
+        if keys[K_UP]:
+            if Wall2.rect.y > 0:
+                Wall2.rect.y -= 5 
+        elif keys[K_DOWN]:
+            if Wall2.rect.y < 410:
+                Wall2.rect.y += 5 
+
 Wall1 = Wall(0, 180, 20, 190)
 Wall2 = Wall(570, 180, 20, 190)
+ball = transform.scale(image.load("мяч.png"), (80, 80))
 
 run = True
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    
-    # Получаем состояние клавиш
-    keys = key.get_pressed()
-    
-    # Управление для Wall1 (W - вверх, S - вниз)
-    if keys[K_w]:
-        if Wall1.rect.y > 0:
-            Wall1.rect.y -= 5  # Изменяем положение на 5 вверх
-    elif keys[K_s]:
-        if Wall1.rect.y < 410:
-            Wall1.rect.y += 5  # Изменяем положение на 5 вниз
-    
-    # Управление для Wall2 (стрелка вверх - вверх, стрелка вниз - вниз)
-    if keys[K_UP]:
-        if Wall2.rect.y > 0:
-            Wall2.rect.y -= 5  # Изменяем положение на 5 вверх
-    elif keys[K_DOWN]:
-        if Wall2.rect.y < 410:
-            Wall2.rect.y += 5  # Изменяем положение на 5 вниз
-    
+            if finish != True:
+                ball.rect.x += speed_x
+                ball.rect.y += speed_y
+            if ball.rect.y > win_height-50 or ball.rect.y < 0:
+                speed_y *= -1
+            if sprite.collide_rect(Wall1, ball) or sprite.collide_rect(Wall2, ball):
+                speed_x *= -1
     window.blit(BG, (0, 0))
+    window.blit(ball, (250, 250))
+    Player1.update()
+    Player2.update()
     Wall1.reset()
     Wall2.reset()
     display.update()
     clock.tick(60)
-
-
-
-
-
--- sharik/pingpong.py
+++ sharik/pingpong.py
@@ -4,6 +4,8 @@ window = display.set_mode((600, 600))
 display.set_caption("ping-pong")
 BG = transform.scale(image.load("background.jpg"), (600, 600))
 clock = time.Clock()
+speed_y = 1
+speed_x = 1
 
 class GameSprite(sprite.Sprite):
     def __init__(self, x, y, w, h, filename, speed=0):
@@ -16,8 +18,9 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
 class Wall(sprite.Sprite):
-    def __init__(self, x, y, w, h, color=(79, 79, 79)):
+    def __init__(self, x, y, w, h, color=(12, 79, 79)):
         super().__init__()
         self.image = Surface((w, h))
         self.image.fill(color)
@@ -27,43 +30,46 @@ class Wall(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player1(GameSprite):
+    def update():
+        keys = key.get_pressed()
+        if keys[K_w]:
+            if Wall1.rect.y > 0:
+                Wall1.rect.y -= 5 
+        elif keys[K_s]:
+            if Wall1.rect.y < 410:
+                Wall1.rect.y += 5 
+class Player2(GameSprite):
+    def update():
+        keys = key.get_pressed()
+        if keys[K_UP]:
+            if Wall2.rect.y > 0:
+                Wall2.rect.y -= 5 
+        elif keys[K_DOWN]:
+            if Wall2.rect.y < 410:
+                Wall2.rect.y += 5 
+
 Wall1 = Wall(0, 180, 20, 190)
 Wall2 = Wall(570, 180, 20, 190)
-x1, y1 = 100, 300
-x2, y2 = 100, 400
+ball = transform.scale(image.load("мяч.png"), (80, 80))
 
 run = True
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    key_pressed = key.get_pressed()
-    if key_pressed[K_w] and y1 > 0:
-        y1 -= 5
-    if key_pressed[K_s] and y1 < 520:
-        y1 += 5
-    if key_pressed[K_d] and x1 < 520:
-        x1 += 5
-    if key_pressed[K_a] and x1 > 0:
-        x1 -= 5
-    
-
-    if key_pressed[K_UP] and y2 > 0:
-        y2 -= 5
-    if key_pressed[K_DOWN] and y2 < 520:
-        y2 += 5
-    if key_pressed[K_RIGHT] and x2 < 520:
-        x2 += 5
-    if key_pressed[K_LEFT] and x2 > 0:
-        x2 -= 5
-
+            if finish != True:
+                ball.rect.x += speed_x
+                ball.rect.y += speed_y
+            if ball.rect.y > win_height-50 or ball.rect.y < 0:
+                speed_y *= -1
+            if sprite.collide_rect(Wall1, ball) or sprite.collide_rect(Wall2, ball):
+                speed_x *= -1
     window.blit(BG, (0, 0))
-    Wall1.reset(x1, y1)
-    Wall2.reset(x2, y2)
+    window.blit(ball, (250, 250))
+    Player1.update()
+    Player2.update()
+    Wall1.reset()
+    Wall2.reset()
     display.update()
     clock.tick(60)
-
-
-
-
-
